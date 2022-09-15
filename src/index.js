4e7688aed909108c1d4925efb76638a2e583b757
Nilton Torres <niltontorresrojas8@gmail.com>
2022-09-14 20:13:27 -0500

    database password

--- src/index.js
+++ src/index.js
@@ -2,6 +2,26 @@ const express = require("express")
 const mongoose = require("mongoose")
 require("dotenv").config();
 const productRoute = require("./routes/product")
+const path = require("path")
+
+//swagger
+const swaggerUI = require("swagger-ui-express")
+const swaggerJsDoc = require("swagger-jsdoc")
+const swaggerSpec = {
+    definition: {
+        openapi:"3.0.0",
+        info:{
+            title: "shop-api",
+            version: "1.0.0"
+        },
+        servers: [
+            {
+                url:"http://localhost:9000"
+            }
+        ]
+    },
+    apis:["./routes/product.js"]
+};
 
 //settings
 const app = express()
@@ -10,10 +30,11 @@ const port = process.env.PORT || 9000;
 //middlewares
 app.use(express.json())
 app.use("/api",productRoute);
+app.use("/api-doc",swaggerUI.serve, swaggerUI.setup(swaggerJsDoc(swaggerSpec)))
 
 //routes
 app.get("/",(req,res)=>{
-    res.send("connected")
+    res.send("welcome")
 });
 
 //mongodb
