91ee4044a706247a497b6db167cadeeea6abba91
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2022-11-05 02:23:07 -0500

    test jwt

---
+++ src/utils/auth/strategies/local.strategies.js
@@ -0,0 +1,25 @@
+const { boom } = require('@hapi/boom')
+const bcrypt = require('bcrypt')
+const { Strategy } = require('passport-local')
+const userController = require('../../../controllers/userController')
+const LocalStrategy = new Strategy({
+  usernameField: 'email',
+  passwordField: 'password'
+},
+async (email, password, done) => {
+  try {
+    const user = await userController.findByEmail(email)
+    if (!user) {
+      done(boom.unauthorized(), false)
+    }
+    const isMatch = await bcrypt.compare(password, user.password)
+    if (!isMatch) {
+      done(boom.unauthorized(), false)
+    }
+    done(null, user)
+  } catch (e) {
+    done(e, false)
+  }
+})
+
+module.exports = LocalStrategy
