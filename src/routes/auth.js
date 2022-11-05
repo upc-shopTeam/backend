e2b4a87cd2a5dacc1dc54fae1906542f7ece455c
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2022-11-05 02:24:03 -0500

    feat: added login

---
+++ src/routes/auth.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const passport = require('passport')
+const jsonwebtoken = require('jsonwebtoken')
+
+const router = express.Router()
+
+router.post('/login',
+  passport.authenticate('local', { session: false }),
+  async (req, res, next) => {
+    try {
+      const user = req.user
+      const payload = {
+        sub: user.id,
+        role: user.role
+      }
+      const token = jsonwebtoken.sign(payload, process.env.JWT_SECRET)
+      res.json({
+        user,
+        token
+      })
+    } catch (e) {
+      next(e)
+    }
+  })
+
+module.exports = router
